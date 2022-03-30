c8d5850f866f7db7a7ef5a174fe46e274c9f1d1b
TaeWhie <xognl427@naver.com>
2022-03-30 17:57:13 +0900

    Optimization Chunk

--- Assets/Script/Chunk.cs
+++ Assets/Script/Chunk.cs
@@ -8,31 +8,106 @@ public class Chunk : MonoBehaviour
 	public MeshRenderer meshRenderer;
 	public MeshFilter meshFilter;
 
+	int vertexIndex = 0;
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Vector2> uvs = new List<Vector2>();
+
+	bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];//복셀 맵 배열의 크기
+
 	void Start()
 	{
+		PopulateVoxelMap();//voxelmap을 true 설정
+		CreateMeshData();//false면 그리고 true면 그리지 않는다.
+		CreateMesh();//렌더링한다.
+	}
+	void PopulateVoxelMap()//Voxelmap의 기본 값을 true로 설정 하는 과정, 이 과정을 거치지 않으면 값은 false이다.
+	{
 
-		int vertexIndex = 0;
-		List<Vector3> vertices = new List<Vector3>();
-		List<int> triangles = new List<int>();
-		List<Vector2> uvs = new List<Vector2>();
-
-		for (int p = 0; p < 6; p++)//VoxelData에 설정해 놓은 값을 통해 값을 집어넣음
+		for (int y = 0; y < VoxelData.ChunkHeight; y++)
 		{
-			for (int i = 0; i < 6; i++)
+			for (int x = 0; x < VoxelData.ChunkWidth; x++)
 			{
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+					//if(x >= y && z >= y) //이런 조건을 추가하면 계단모양의 Chunk를 만들수 있다.
+					voxelMap[x, y, z] = true;
 
-				int triangleIndex = VoxelData.voxelTris[p, i];
-				vertices.Add(VoxelData.voxelVerts[triangleIndex]);
-				triangles.Add(vertexIndex);
-
-				uvs.Add(VoxelData.voxelUvs[i]);
-
-				vertexIndex++;
-
+				}
 			}
 		}
 
-		Mesh mesh = new Mesh();//mesh를 이용하여 그림
+	}
+
+	void CreateMeshData()//Chunkmap을 그리기 위해서 Voxel하나 하나 값을 집어넣는 과정
+	{
+
+		for (int y = 0; y < VoxelData.ChunkHeight; y++)
+		{
+			for (int x = 0; x < VoxelData.ChunkWidth; x++)
+			{
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+
+					AddVoxelDataToChunk(new Vector3(x, y, z));
+
+				}
+			}
+		}
+
+	}
+
+	bool CheckVoxel(Vector3 pos)
+	{
+
+		int x = Mathf.FloorToInt(pos.x);
+		int y = Mathf.FloorToInt(pos.y);
+		int z = Mathf.FloorToInt(pos.z);
+		// 맵 범위를 벗어나는 경우
+		if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+			return false;
+
+		return voxelMap[x, y, z];
+
+	}
+
+	void AddVoxelDataToChunk(Vector3 pos)
+	{
+		for (int p = 0; p < 6; p++)//VoxelData에 설정해 놓은 값을 통해 값을 집어넣음
+		{	 //각 면을 그리는 과정이다. 정육면체임으로 6번을 순환한다.
+
+			 // Face Check(면이 바라보는 방향으로 +1 이동하여 확인)를 했을 때 
+			 // Solid가 아닌 경우에만 큐브의 면이 그려지도록 하기
+			 // => 청크의 외곽 부분만 면이 그려지고, 내부에는 면이 그려지지 않도록
+			if (CheckVoxel(pos) && !CheckVoxel(pos + VoxelData.faceChecks[p]))
+			{
+				// 각 면(삼각형 2개) 그리기
+
+				// 1. Vertex, UV 4개 추가
+				for (int i = 0; i <= 3; i++)
+				{
+					vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, i]] + pos);
+					uvs.Add(VoxelData.voxelUvs[i]);
+				}
+
+				// 2. Triangle의 버텍스 인덱스 6개 추가
+				triangles.Add(vertexIndex);
+				triangles.Add(vertexIndex + 1);
+				triangles.Add(vertexIndex + 2);
+
+				triangles.Add(vertexIndex + 2);
+				triangles.Add(vertexIndex + 1);
+				triangles.Add(vertexIndex + 3);
+
+				vertexIndex += 4;
+			}
+		}
+	}
+
+	void CreateMesh()
+	{
+
+		Mesh mesh = new Mesh();
 		mesh.vertices = vertices.ToArray();
 		mesh.triangles = triangles.ToArray();
 		mesh.uv = uvs.ToArray();
@@ -42,5 +117,4 @@ public class Chunk : MonoBehaviour
 		meshFilter.mesh = mesh;
 
 	}
-
 }
--- Assets/Script/VoxelData.cs
+++ Assets/Script/VoxelData.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public static class VoxelData//복셀에 들어갈 하나의 큐브를 설정하는 작업
 {
-
+	//덩어리의 크기를 지정해 준다.
+	public static readonly int ChunkWidth = 5;
+	public static readonly int ChunkHeight = 15;
 
 	public static readonly Vector3[] voxelVerts = new Vector3[8] {//정육면체의 각각 정점의 위치
 
@@ -19,27 +21,49 @@ public static class VoxelData//복셀에 들어갈 하나의 큐브를 설정하
 
 	};
 
-	public static readonly int[,] voxelTris = new int[6, 6] {//각각의 면을 그리는 순서
-
-		{0, 3, 1, 1, 3, 2}, // Back Face
-		{5, 6, 4, 4, 6, 7}, // Front Face
-		{3, 7, 2, 2, 7, 6}, // Top Face
-		{1, 5, 0, 0, 5, 4}, // Bottom Face
-		{4, 7, 0, 0, 7, 3}, // Left Face
-		{1, 2, 5, 5, 2, 6} // Right Face
-
+	public static readonly int[,] voxelTris = new int[6, 4] {//각각의 면을 그리는 순서
+		//최적화 작업을 위하여 진행한 작업
+		/*LT ㅡㅡㅡ RT		
+		 * l	   l		첫번째 삼각형은 [LB - LT - RB], 두 번째 삼각형은 [RB - LT - RT]
+		 * l	   l		하지만 실제 사각형은 정점 6개가 아니라 4개 만으로 정의 할 수 있기 때문에
+		 * l	   l		배열도 4개로 바꾸어준다.
+		 *LB ㅡㅡㅡ RB
+		 */
+		/*{0, 3, 1, 1, 3, 2},->*/{0, 3, 1, 2}, // Back Face
+		/*{5, 6, 4, 4, 6, 7},->*/{5, 6, 4, 7}, // Front Face
+		/*{3, 7, 2, 2, 7, 6},->*/{3, 7, 2, 6}, // Top Face
+		/*{1, 5, 0, 0, 5, 4},->*/{1, 5, 0, 4}, // Bottom Face
+		/*{4, 7, 0, 0, 7, 3},->*/{4, 7, 0, 3}, // Left Face
+		/*{1, 2, 5, 5, 2, 6} ->*/{1, 2, 5, 6} // Right Face
+		
 	};
 
-	public static readonly Vector2[] voxelUvs = new Vector2[6] {//texture의 uv에 따라 큐브 각각의 면에 값 할당
+	public static readonly Vector2[] voxelUvs = new Vector2[4] {//texture의 uv에 따라 큐브 각각의 면에 값 할당
+
+		//UV도 마찬가지로 6개의 점이 아닌 4개에 점에 할당해도 됨으로 크기를 줄인다.
+		/*new Vector2 (0.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 1.0f)*/
+
 
 		new Vector2 (0.0f, 0.0f),
 		new Vector2 (0.0f, 1.0f),
 		new Vector2 (1.0f, 0.0f),
-		new Vector2 (1.0f, 0.0f),
-		new Vector2 (0.0f, 1.0f),
 		new Vector2 (1.0f, 1.0f)
-
 	};
 
+	public static readonly Vector3[] faceChecks = new Vector3[6] {//voxelTris를 참조하여,각 면마다 생기는 수직 벡터의 값이다.
+
+		new Vector3(0.0f, 0.0f, -1.0f),
+		new Vector3(0.0f, 0.0f, 1.0f),
+		new Vector3(0.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, -1.0f, 0.0f),
+		new Vector3(-1.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 0.0f, 0.0f)
+
+	};
 
 }
